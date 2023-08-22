a8d23e1654a336163e866f3bc6c6306099e36bf5
sameedsk20 <138784854+sameedsk20@users.noreply.github.com>
2023-08-22 16:56:08 +0500

    Tribute Page - Imran Khan
    This is a page built in tribute of Imran Khan using  HTML, CSS , JS.

---
+++ script.js
@@ -0,0 +1,39 @@
+window.addEventListener("scroll",reveal);
+
+function reveal(){
+
+var reveals = document.querySelectorAll('.reveal');
+
+for(var i = 0 ; i < reveals.length; i++){
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 50;
+
+    if(revealTop < windowHeight - revealPoint){
+        reveals[i].classList.add('active');
+    }
+    else{
+        reveals[i].classList.remove('active');
+    }
+  }
+}
+
+window.addEventListener("scroll",revealXR);
+
+function revealXR(){
+
+var reveals = document.querySelectorAll('.revealXR');
+
+for(var i = 0 ; i < reveals.length; i++){
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if(revealTop < windowHeight - revealPoint){
+        reveals[i].classList.add('active');
+    }
+    else{
+        reveals[i].classList.remove('active');
+    }
+  }
+}
